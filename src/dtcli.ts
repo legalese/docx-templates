3fffb32b4b1aaced6fa5040d2dfc406ef840cf85
Job Chong <jobchong@gmail.com>
2020-06-03 19:54:42 +0800

    use the provided build tools for dtcli

--- src/dtcli.ts
+++ src/dtcli.ts
@@ -1,19 +1,22 @@
 
 // trivial CLI useful for testing
 
-// usage: dtcli myTemplate.docx myContext.json myOutfile.docx
+// usage:
+// npm run-script compile
+// node lib/dtcli.js myTemplate.docx myContext.json myOutfile.docx
 
-import createReport from 'docx-templates';
-import fs from 'fs';
+import createReport from './index.js';
+const fs = require('fs');
 
-const template = fs.readFileSync('myTemplate.docx');
+const template = fs.readFileSync(process.argv[2]);
+const context = fs.readFileSync(process.argv[3], 'utf-8');
 
-const buffer = await createReport({
+const buffer = createReport({
   template,
-  data: {
-    name: 'John',
-    surname: 'Appleseed',
-  },
-});
-
-fs.writeFileSync('report.docx', buffer)
+  data: JSON.parse(context),
+}).then(function(data:any) {
+  console.log('done')
+  fs.writeFileSync(process.argv[4], data)
+}).catch(function(error:any) {
+  console.log(error)
+})
